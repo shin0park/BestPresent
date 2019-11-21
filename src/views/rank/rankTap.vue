7ba17abc2e65bb4b1d128e2a258e61cea396e037
Yongjae Park <dydwo0710@ajou.ac.kr>
2019-11-21 21:59:39 +0900

    광고 삽입

--- src/views/rank/rankTap.vue
+++ src/views/rank/rankTap.vue
@@ -2,6 +2,9 @@
     <div>
         <section class="categoryTap">
             <situationSection></situationSection>
+            <div class="adBox">
+                <img src="../../assets/ad.png">
+            </div>
             <rankSection></rankSection>
         </section>
     </div>
@@ -28,5 +31,11 @@
         padding: 0 16px;
         width: 100%;
         box-sizing: border-box;
+    }
+    .adBox {
+        margin: 20px 0;
+    }
+    .adBox img {
+        width: 100%;
     }
 </style>
